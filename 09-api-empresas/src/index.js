32bf3dc07c0e61b5f82ceed59e4c0c5a2abab2d0
Yguin77 <fariasygor234@gmail.com>
2025-10-30 20:09:52 -0300

    oi

---
+++ 09-api-empresas/src/index.js
@@ -0,0 +1,25 @@
+const express = require ("express")
+const app = express()
+
+app.use(express.json)
+
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASS = process.env.DB_PASS
+const DB_NAME = process.env.DB_NAME
+
+const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(url)
+  .then(() => {
+    console.log("Conectado ao MongoDB")
+  })
+  .catch(err => {
+    console.log("Erro ao conectar no banco MongoDB: ", err)
+  })
+
+
+  app.listen
